5f8c0767d940ca2b6bd71601256d5bba914144e8
Your Name <al.james@gmail.com>
2010-06-01 23:53:47 +0100

    Improved demo.php

--- demo.php
+++ demo.php
@@ -1,22 +1,41 @@
 <?php
-
+/**
+ *	Include the API PHP file
+ */
 require('php-neo-rest.php');
 
+/**
+ *	Create a graphDb connection 
+ *	Note:	this does not actually perform any network access, 
+ *			the server is only accessed when you use the database
+ */
 $graphDb = new GraphDatabaseService('http://localhost:9999/');
 
+/**
+ *	Lets create some nodes
+ *	Note: Unlike the java API, these nodes are NOT saved until you call the save() method (see below)
+ */
 $firstNode = $graphDb->createNode();
 $secondNode = $graphDb->createNode();
 $thirdNode = $graphDb->createNode();
 
+/**
+ *	Assign some attributes to the nodes and save the,
+ */
 $firstNode->message = "Hello, ";
 $firstNode->save();
 
 $secondNode->message = "world!";
+$secondNode->someOtherAttribute = 'blah blah blah';
 $secondNode->save();
 
 $thirdNode->message = "third node";
 $thirdNode->save();
 
+/**
+ *	Create a relationship between some nodes. These can also have attributes.
+ *	Note: Relationships also need to be saved before they exist in the DB.
+ */
 $relationship = $firstNode->createRelationshipTo($secondNode, 'KNOWS');
 $relationship->message = "brave Neo4j";
 $relationship->save();
@@ -24,12 +43,17 @@ $relationship->save();
 $relationship2 = $thirdNode->createRelationshipTo($secondNode, 'LOVES');
 $relationship2->save();
 
-
+/**
+ *	Dump each node we created
+ */
 dump_node($firstNode);
 dump_node($secondNode);
 dump_node($thirdNode);
 
 
+/**
+ *	A little utility function to display a node
+ */
 function dump_node($node)
 {
 	$rels = $node->getRelationships();
